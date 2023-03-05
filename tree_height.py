5e258bf4e03ee352f258582f24c9df9aff125079
Top4ak <120311720+Top4ak@users.noreply.github.com>
2023-03-05 23:45:22 +0200

    221RDB014 Mihails Ruhļa

--- tree_height.py
+++ tree_height.py
@@ -1,33 +1,57 @@
-# python3
-
+#221RDB014 Mihails Ruhļa 13. grupa
 import sys
+from typing import List, Tuple
 import threading
-import numpy
 
+input = sys.stdin.readline
 
-def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+ll = int
+ld = float
+pt = Tuple[ll, ll]
 
+def dfs(v: ll, pa: ll, path: ll, mx: List[ll], g: List[List[ll]]) -> None:
+    mx[v] = path
+    for i in g[v]:
+        if mx[i] > 0:
+            continue
+        dfs(i, v, path + 1, mx, g)
 
-def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
+def main() -> None:
+    mode = input()
+    ans = 0
+    mx = [0] * 107
+    n = 0
+    root = 0
+    g = [[] for _ in range(107)]
+    if "F" in mode:
+        filename = input()
+        if"a" not in filename:
+            with open(str("test/"+filename), mode ="r") as f:
+                n = int(f.readline())
+                parents = list(map(int, f.readline().split()))
+                for i, j in enumerate(parents):
+                    if j == -1:
+                        root = i
+                        continue
+                    g[i].append(j)
+                    g[j].append(i)
+    else:
+        n = ll(input())
+        for i in range(n):
+            v = ll(input())
+            if v == -1:
+                root = i
+                continue
+            g[v].append(i)
+            g[i].append(v)
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
+    dfs(root, -1, 1, mx, g)
+
+    for i in range(n):
+        ans = max(ans, mx[i])
+
+    print(ans)
+
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))
